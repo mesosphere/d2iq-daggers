2400804e273a3a451d484282702bf5072eb95013
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-01 16:09:08 +0000

    feat: Use pre-commit zipapp

--- dagger/precommit/dagger.go
+++ dagger/precommit/dagger.go
@@ -8,35 +8,30 @@ import (
 	"dagger.io/dagger"
 )
 
-//go:embed scripts/install_dependencies.sh
-var InstallDependenciesScript string
-
 func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory) error {
-	// Create a directory for scripts we need to use in container
-	scriptsDir := client.
-		Directory().
-		WithNewFile("install_dependencies.sh", dagger.DirectoryWithNewFileOpts{Contents: InstallDependenciesScript})
-
-	scriptsDirID, err := scriptsDir.ID(ctx)
-	if err != nil {
-		return err
-	}
-
-	workdirID, err := workdir.ID(ctx)
+	srcDirID, err := workdir.ID(ctx)
 	if err != nil {
 		return err
 	}
 
 	// Create a pre-commit container
 	container := client.
-		Container().
-		From("debian:bullseye-slim").
-		WithMountedDirectory("/scripts", scriptsDirID).
-		WithMountedDirectory("/work", workdirID).
-		WithWorkdir("/work").
-		WithEntrypoint([]string{"bash"}).
-		Exec(dagger.ContainerExecOpts{Args: []string{"/scripts/install_dependencies.sh"}}).
-		Exec(dagger.ContainerExecOpts{Args: []string{"-c", "pre-commit", "run -a --show-diff-on-failure"}})
+		Container().From("python:3.12.0a1-bullseye").
+		WithMountedDirectory("/src", srcDirID).WithWorkdir("/src").
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{
+				"curl",
+				"--location", "--fail", "--silent", "--show-error",
+				"--output", "/usr/local/bin/pre-commit-2.20.0.pyz",
+				"https://github.com/pre-commit/pre-commit/releases/download/v2.20.0/pre-commit-2.20.0.pyz",
+			},
+		}).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{
+				"python", "/usr/local/bin/pre-commit-2.20.0.pyz",
+				"run", "--all-files", "--show-diff-on-failure",
+			},
+		})
 
 	// Run container and get Exit code
 	_, err = container.ExitCode(ctx)
