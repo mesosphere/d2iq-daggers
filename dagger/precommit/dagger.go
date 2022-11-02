fb1f861d9267ddf0c033ddf68d1a210f9b10b98c
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-02 09:32:28 +0000

    fix: Cache install hooks in earlier layer

--- dagger/precommit/dagger.go
+++ dagger/precommit/dagger.go
@@ -8,8 +8,19 @@ import (
 	"dagger.io/dagger"
 )
 
+const (
+	precommitConfigFileName              = ".pre-commit-config.yaml"
+	precommitInstallHooksDir             = "/tmp/install-pre-commit-hooks/"
+	precommitInstallHooksMountConfigFile = precommitInstallHooksDir + precommitConfigFileName
+)
+
 func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory) error {
 	srcDirID, err := workdir.ID(ctx)
+	if err != nil {
+		return err
+	}
+	precommitConfigFile := workdir.File(precommitConfigFileName)
+	precommitConfigFileID, err := precommitConfigFile.ID(ctx)
 	if err != nil {
 		return err
 	}
@@ -17,7 +28,6 @@ func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory) 
 	// Create a pre-commit container
 	container := client.
 		Container().From("python:3.12.0a1-bullseye").
-		WithMountedDirectory("/src", srcDirID).WithWorkdir("/src").
 		Exec(dagger.ContainerExecOpts{
 			Args: []string{
 				"curl",
@@ -26,12 +36,20 @@ func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory) 
 				"https://github.com/pre-commit/pre-commit/releases/download/v2.20.0/pre-commit-2.20.0.pyz",
 			},
 		}).
+		WithMountedFile(precommitInstallHooksMountConfigFile, precommitConfigFileID).WithWorkdir(precommitInstallHooksDir).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{
+				"git", "init",
+			},
+		}).
 		Exec(dagger.ContainerExecOpts{
 			Args: []string{
 				"python", "/usr/local/bin/pre-commit-2.20.0.pyz",
 				"install-hooks",
 			},
 		}).
+		WithoutMount(precommitInstallHooksMountConfigFile).
+		WithMountedDirectory("/src", srcDirID).WithWorkdir("/src").
 		Exec(dagger.ContainerExecOpts{
 			Args: []string{
 				"python", "/usr/local/bin/pre-commit-2.20.0.pyz",
