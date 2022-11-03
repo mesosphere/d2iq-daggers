d29c2fbe1e572cbe02b8e343a95f1a2f251aef3e
Ali AKCA <ali@akca.io>
2022-11-03 10:25:42 +0100

    feat: add dagger svu (#10)
    This dagger action allows us to generate a version from the work
    directory with the given configuration.
    
    ```go
    func GenVersion(ctx context.Context) error {
    	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
    	if err != nil {
    		panic(err)
    	}
    	defer client.Close()
    
    	output, err := svu.Run(ctx, client, client.Host().Workdir().Read(), svu.WithSuffix("-dev"))
    	if err != nil {
    		return err
    	}
    
    	fmt.Println(output.Version)
    	fmt.Println(output.VersionWithoutPrefix)
    
    	return nil
    }
    
    ```
    
    Co-authored-by: Jimmi Dyson <jimmidyson@gmail.com>

---
+++ dagger/svu/dagger.go
@@ -0,0 +1,104 @@
+package svu
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"dagger.io/dagger"
+)
+
+const (
+	baseImage = "ghcr.io/caarlos0/svu"
+)
+
+// Output is svu command output
+type Output struct {
+	// Version
+	Version string
+	// Version without the prefix
+	VersionWithoutPrefix string
+}
+
+func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory, options ...Option) (*Output, error) {
+	cfg := defaultConfig()
+	for _, o := range options {
+		cfg = o(cfg)
+	}
+
+	svuFlags := flagsFromConfig(cfg)
+
+	srcDirID, err := workdir.ID(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	container := client.Container().
+		From(fmt.Sprintf("%s:%s", baseImage, cfg.version)).
+		WithMountedDirectory("/src", srcDirID).
+		WithWorkdir("/src")
+
+	container = container.Exec(dagger.ContainerExecOpts{Args: append([]string{string(cfg.command)}, svuFlags...)})
+	// Run container and get Exit code
+	_, err = container.ExitCode(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	version, err := container.Stdout().Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	svuFlags = append(svuFlags, "--strip-prefix")
+	container = container.Exec(dagger.ContainerExecOpts{Args: append([]string{string(cfg.command)}, svuFlags...)})
+	// Run container and get Exit code
+	_, err = container.ExitCode(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	versionWithoutPrefix, err := container.Stdout().Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Output{
+		Version:              strings.TrimSpace(version),
+		VersionWithoutPrefix: strings.TrimSpace(versionWithoutPrefix),
+	}, nil
+}
+
+func flagsFromConfig(cfg config) []string {
+	var flags []string
+
+	if cfg.pattern != "" {
+		flags = append(flags, "--pattern", cfg.pattern)
+	}
+	if cfg.prefix != "" {
+		flags = append(flags, "--prefix", cfg.prefix)
+	}
+	if cfg.suffix != "" {
+		flags = append(flags, "--suffix", cfg.suffix)
+	}
+	if cfg.tagMode != "" {
+		flags = append(flags, "--tag-mode", string(cfg.tagMode))
+	}
+	if cfg.metadata {
+		flags = append(flags, "--metadata")
+	} else {
+		flags = append(flags, "--no-metadata")
+	}
+	if cfg.prerelease {
+		flags = append(flags, "--pre-release")
+	} else {
+		flags = append(flags, "--no-pre-release")
+	}
+	if cfg.build {
+		flags = append(flags, "--build")
+	} else {
+		flags = append(flags, "--no-build")
+	}
+
+	return flags
+}
---
+++ dagger/svu/options.go
@@ -0,0 +1,129 @@
+package svu
+
+// Command is represents the svu sub-command.
+type Command string
+
+const (
+	// CommandNext is the svu next sub-command.
+	CommandNext Command = "next"
+	// CommandMajor is the svu major sub-command.
+	CommandMajor Command = "major"
+	// CommandMinor is the svu minor sub-command.
+	CommandMinor Command = "minor"
+	// CommandPatch is the svu patch sub-command.
+	CommandPatch Command = "patch"
+	// CommandCurrent is the svu pre sub-command.
+	CommandCurrent Command = "current"
+)
+
+// TagMode is a custom type representing the possible values for the --tag-mode flag.
+type TagMode string
+
+const (
+	// TagModeAllBranches is the value for the --tag-mode flag that will use all branches.
+	TagModeAllBranches TagMode = "all-branches"
+	// TagModeCurrentBranch is the value for the --tag-mode flag that will only use current branch tags
+	TagModeCurrentBranch TagMode = "current-branch"
+)
+
+type config struct {
+	version    string
+	metadata   bool
+	prerelease bool
+	build      bool
+	command    Command
+	pattern    string
+	prefix     string
+	suffix     string
+	tagMode    TagMode
+}
+
+func defaultConfig() config {
+	return config{
+		version:    "v1.9.0",
+		metadata:   true,
+		prerelease: true,
+		build:      true,
+		command:    CommandNext,
+		pattern:    "*",
+		prefix:     "v",
+		suffix:     "",
+		tagMode:    TagModeAllBranches,
+	}
+}
+
+type Option func(config) config
+
+// SVUVersion specifies the version of svu to use. Defaults to v1.9.0. This should be one of the
+// released image tags - see https://github.com/caarlos0/svu/pkgs/container/svu for available
+// tags.
+func SVUVersion(v string) Option {
+	return func(c config) config {
+		c.version = v
+		return c
+	}
+}
+
+// WithMetadata controls whether to include pre-release and build metadata in the version. Defaults to true.
+func WithMetadata(b bool) Option {
+	return func(c config) config {
+		c.metadata = b
+		return c
+	}
+}
+
+// WithPreRelease controls whether to include pre-release metadata in the version. Defaults to true.
+func WithPreRelease(b bool) Option {
+	return func(c config) config {
+		c.prerelease = b
+		return c
+	}
+}
+
+// WithBuild controls whether to include build metadata in the version. Defaults to true.
+func WithBuild(b bool) Option {
+	return func(c config) config {
+		c.build = b
+		return c
+	}
+}
+
+// WithCommand sets the svu sub-command to run. Defaults to "next".
+func WithCommand(cmd Command) Option {
+	return func(c config) config {
+		c.command = cmd
+		return c
+	}
+}
+
+// WithPattern sets the pattern to use when searching for tags. Defaults to "*".
+func WithPattern(pattern string) Option {
+	return func(c config) config {
+		c.pattern = pattern
+		return c
+	}
+}
+
+// WithPrefix sets the prefix to use when searching for tags. Defaults to "v".
+func WithPrefix(prefix string) Option {
+	return func(c config) config {
+		c.prefix = prefix
+		return c
+	}
+}
+
+// WithSuffix sets the suffix to use when searching for tags. Defaults to "".
+func WithSuffix(suffix string) Option {
+	return func(c config) config {
+		c.suffix = suffix
+		return c
+	}
+}
+
+// WithTagMode sets the tag mode to use when searching for tags. Defaults to TagModeAllBranches.
+func WithTagMode(tagMode TagMode) Option {
+	return func(c config) config {
+		c.tagMode = tagMode
+		return c
+	}
+}
