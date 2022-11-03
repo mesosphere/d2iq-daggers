2ee1e36d87300e24273854682cd379fc950ccebd
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-03 10:12:10 +0000

    fix: Remove redundant dagger client creation (#15)

--- mage/svu/mage.go
+++ mage/svu/mage.go
@@ -16,56 +16,26 @@ const (
 
 // Current runs svu current.
 func Current(ctx context.Context) error {
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandCurrent))
 }
 
 // Next runs svu next.
 func Next(ctx context.Context) error {
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandNext))
 }
 
 // Major runs svu major.
 func Major(ctx context.Context) error {
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMajor))
 }
 
 // Minor runs svu minor.
 func Minor(ctx context.Context) error {
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMinor))
 }
 
 // Patch runs svu patch.
 func Patch(ctx context.Context) error {
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandPatch))
 }
 
