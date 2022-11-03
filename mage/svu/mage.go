fe35abaa6547e7a70b699ac04929b81890bb6661
aweris <ali@akca.io>
2022-11-03 16:48:57 +0100

    feat: Add configuration options to svu via env

--- mage/svu/mage.go
+++ mage/svu/mage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 
 	"dagger.io/dagger"
 
@@ -14,7 +15,14 @@ import (
 )
 
 const (
-	svuVersionEnvVar = "SVU_VERSION"
+	svuVersionEnvVar    = "SVU_VERSION"
+	svuMetadataEnvVar   = "SVU_METADATA"
+	svuPatternEnvVar    = "SVU_PATTERN"
+	svuPreReleaseEnvVar = "SVU_PRERELEASE"
+	svuBuildEnvVar      = "SVU_BUILD"
+	svuPrefixEnvVar     = "SVU_PREFIX"
+	svuSuffixEnvVar     = "SVU_SUFFIX"
+	svuTagModeEnvVar    = "SVU_TAG_MODE"
 )
 
 // Current runs svu current.
@@ -57,11 +65,19 @@ func SVUWithOptions(ctx context.Context, opts ...svudagger.Option) error {
 	}
 	defer client.Close()
 
-	if svuVersion, ok := os.LookupEnv(svuVersionEnvVar); ok {
-		opts = append([]svudagger.Option{svudagger.SVUVersion(svuVersion)}, opts...)
+	optsFromEnv, err := loadEnvVars()
+	if err != nil {
+		return err
 	}
 
-	output, err := svudagger.Run(ctx, client, client.Host().Workdir().Read(), opts...)
+	// Combine options from environment variables and options passed to this function. Environment variables
+	// take precedence to allow overriding from the arguments passed to this function.
+	var combinedOpts []svudagger.Option
+
+	combinedOpts = append(combinedOpts, optsFromEnv...)
+	combinedOpts = append(combinedOpts, opts...)
+
+	output, err := svudagger.Run(ctx, client, client.Host().Workdir().Read(), combinedOpts...)
 	if err != nil {
 		return err
 	}
@@ -69,4 +85,55 @@ func SVUWithOptions(ctx context.Context, opts ...svudagger.Option) error {
 	fmt.Println(output.Version)
 
 	return nil
+}
+
+// loads environment variables into options.
+func loadEnvVars() ([]svudagger.Option, error) {
+	var opts []svudagger.Option
+
+	if svuVersion, ok := os.LookupEnv(svuVersionEnvVar); ok {
+		opts = append([]svudagger.Option{svudagger.SVUVersion(svuVersion)}, opts...)
+	}
+
+	if svuMetadata, ok := os.LookupEnv(svuMetadataEnvVar); ok {
+		metadata, err := strconv.ParseBool(svuMetadata)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse %q as a boolean: %w", svuMetadataEnvVar, err)
+		}
+		opts = append([]svudagger.Option{svudagger.WithMetadata(metadata)}, opts...)
+	}
+
+	if svuPreRelease, ok := os.LookupEnv(svuPreReleaseEnvVar); ok {
+		preRelease, err := strconv.ParseBool(svuPreRelease)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse %q as a boolean: %w", svuPreReleaseEnvVar, err)
+		}
+		opts = append([]svudagger.Option{svudagger.WithPreRelease(preRelease)}, opts...)
+	}
+
+	if svuBuild, ok := os.LookupEnv(svuBuildEnvVar); ok {
+		build, err := strconv.ParseBool(svuBuild)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse %q as a boolean: %w", svuBuildEnvVar, err)
+		}
+		opts = append([]svudagger.Option{svudagger.WithBuild(build)}, opts...)
+	}
+
+	if svuPattern, ok := os.LookupEnv(svuPatternEnvVar); ok {
+		opts = append([]svudagger.Option{svudagger.WithPattern(svuPattern)}, opts...)
+	}
+
+	if svuPrefix, ok := os.LookupEnv(svuPrefixEnvVar); ok {
+		opts = append([]svudagger.Option{svudagger.WithPrefix(svuPrefix)}, opts...)
+	}
+
+	if svuSuffix, ok := os.LookupEnv(svuSuffixEnvVar); ok {
+		opts = append([]svudagger.Option{svudagger.WithSuffix(svuSuffix)}, opts...)
+	}
+
+	if svuTagMode, ok := os.LookupEnv(svuTagModeEnvVar); ok {
+		opts = append([]svudagger.Option{svudagger.WithTagMode(svudagger.TagMode(svuTagMode))}, opts...)
+	}
+
+	return opts, nil
 }
