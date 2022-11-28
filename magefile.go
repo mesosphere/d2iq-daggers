a49d7c222d8580a129c8f9d5af016fec7ff57e03
aweris <ali@akca.io>
2022-11-28 13:46:16 +0100

    test: use dagger/golang to run tests

--- magefile.go
+++ magefile.go
@@ -3,16 +3,44 @@
 package main
 
 import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+
 	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
+
+	"github.com/mesosphere/daggers/dagger/golang"
 )
 
-var env = map[string]string{"GOPRIVATE": "github.com/mesosphere"}
+// Test is a collection of test targets.
+//
+//goland:noinspection GoUnusedExportedType // used by mage
+type Test mg.Namespace
 
-type (
-	Test mg.Namespace
-)
+func (Test) Go(ctx context.Context) error {
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout), dagger.WithWorkdir("."))
+	if err != nil {
+		return err
+	}
 
-func (Test) Go() error {
-	return sh.RunWithV(env, "go", "test", "-v", "-race", "-coverprofile", "coverage.txt", "-covermode", "atomic", "./...")
+	args := []string{"test", "-v", "-race", "-coverprofile", "coverage.txt", "-covermode", "atomic", "./..."}
+
+	_, dir, err := golang.RunCommand(
+		ctx,
+		client,
+		client.Host().Directory("."),
+		golang.WithArgs(args...),
+		golang.WithEnv(map[string]string{"GOPRIVATE": "github.com/mesosphere"}),
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = dir.File("coverage.txt").Export(ctx, ".output/coverage.txt")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
