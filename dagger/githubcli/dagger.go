21b2df782aebc4230bf28b33079b1bb0536d4b29
Ali AKCA <ali@akca.io>
2022-11-28 14:27:09 +0300

    feat: Add github-cli dagger package (#38)
    feat: Add github cli dagger package

---
+++ dagger/githubcli/dagger.go
@@ -0,0 +1,102 @@
+package githubcli
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"dagger.io/dagger"
+
+	"github.com/mesosphere/daggers/dagger/options"
+)
+
+const (
+	// url template for downloading github cli from github releases.
+	ghURLTemplate = "https://github.com/cli/cli/releases/download/v%s/gh_%s_linux_amd64.tar.gz"
+
+	// standard source path.
+	srcDir = "/src"
+)
+
+// Run runs the ginkgo run command with given options.
+func Run(ctx context.Context, client *dagger.Client, workdir *dagger.Directory, opts ...Option) (string, error) {
+	cfg, err := loadConfigFromEnv()
+	if err != nil {
+		return "", err
+	}
+
+	for _, o := range opts {
+		cfg = o(cfg)
+	}
+
+	container, err := getGHContainer(ctx, client, workdir, &cfg)
+	if err != nil {
+		return "", err
+	}
+
+	container = container.
+		WithMountedDirectory(srcDir, workdir).
+		WithWorkdir(srcDir).
+		WithEnvVariable("CACHE_BUSTER", time.Now().String()). // Workaround for stop caching after this step
+		Exec(dagger.ContainerExecOpts{Args: cfg.Args})
+
+	output, err := container.Stdout().Contents(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(output), nil
+}
+
+// getGHContainer returns a dagger container instance with github cli as entrypoint.
+func getGHContainer(
+	ctx context.Context, client *dagger.Client, workdir *dagger.Directory, cfg *config,
+) (*dagger.Container, error) {
+	var err error
+
+	// Source url for downloading the Github CLI
+	srcURL := fmt.Sprintf(ghURLTemplate, cfg.GithubCliVersion, cfg.GithubCliVersion)
+
+	// Destination file to download tar file contains Github CLI
+	dstFile := "/tmp/gh_linux_amd64.tar.gz"
+
+	// Extract Directory
+	extractDir := "/tmp"
+
+	// Cli path after extracting downloaded tar to extract directory
+	cliPath := fmt.Sprintf("/tmp/gh_%s_linux_amd64/bin/gh", cfg.GithubCliVersion)
+
+	var customizers []options.ContainerCustomizer
+
+	customizers = append(customizers, options.WithMountedGoCache(ctx, workdir), options.DownloadFile(srcURL, dstFile))
+
+	container := client.Container().From(fmt.Sprintf("%s:%s", cfg.GoBaseImage, cfg.GoVersion))
+
+	for _, customizer := range customizers {
+		container, err = customizer(container, client)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	token := client.Host().EnvVariable("GITHUB_TOKEN").Secret()
+
+	container = container.
+		WithSecretVariable("GITHUB_TOKEN", token).
+		Exec(dagger.ContainerExecOpts{Args: []string{"tar", "-xf", dstFile, "-C", extractDir}}).
+		Exec(dagger.ContainerExecOpts{Args: []string{"mv", cliPath, "/usr/local/bin/gh"}}).
+		Exec(dagger.ContainerExecOpts{Args: []string{"rm", "-rf", "/tmp/*"}}).
+		WithEntrypoint([]string{"/usr/local/bin/gh"})
+
+	for _, extension := range cfg.Extensions {
+		container = container.Exec(dagger.ContainerExecOpts{Args: []string{"extension", "install", extension}})
+	}
+
+	_, err = container.ExitCode(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return container, nil
+}
