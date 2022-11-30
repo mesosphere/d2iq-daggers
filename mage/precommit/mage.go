f2f07ed0819fb5f705d2b3f4eb781b6cd483b977
aweris <ali@akca.io>
2022-11-30 14:17:47 +0100

    refactor: move back svu and precommit methods to mage

--- mage/precommit/mage.go
+++ mage/precommit/mage.go
@@ -3,10 +3,32 @@ package precommit
 import (
 	"context"
 
+	"github.com/mesosphere/daggers/daggers"
+
 	precommitdagger "github.com/mesosphere/daggers/dagger/precommit"
 )
 
 // Precommit runs all the precommit checks. Run `mage help:precommit` for information on available options.
 func Precommit(ctx context.Context) error {
-	return precommitdagger.PrecommitWithOptions(ctx)
+	return PrecommitWithOptions(ctx)
+}
+
+// PrecommitWithOptions runs all the precommit checks with Dagger options.
+//
+//nolint:revive // Stuttering is fine here to provide a functional options variant of Precommit function above.
+func PrecommitWithOptions(ctx context.Context, opts ...daggers.Option[precommitdagger.Config]) error {
+	runtime, err := daggers.NewRuntime(ctx, daggers.WithVerbose(true))
+	if err != nil {
+		return err
+	}
+	defer runtime.Client.Close()
+
+	// Print the command output to stdout when the issue https://github.com/dagger/dagger/issues/3192. is fixed.
+	// Currently, we set verbose to true to see the output of the command.
+	_, err = precommitdagger.Run(ctx, runtime, opts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- mage/svu/mage.go
+++ mage/svu/mage.go
@@ -2,31 +2,59 @@ package svu
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/magefile/mage/mg"
 
 	svudagger "github.com/mesosphere/daggers/dagger/svu"
+	"github.com/mesosphere/daggers/daggers"
 )
 
 // Current runs svu current.
 func Current(ctx context.Context) error {
-	return svudagger.SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandCurrent))
+	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandCurrent))
 }
 
 // Next runs svu next.
 func Next(ctx context.Context) error {
-	return svudagger.SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandNext))
+	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandNext))
 }
 
 // Major runs svu major.
 func Major(ctx context.Context) error {
-	return svudagger.SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMajor))
+	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMajor))
 }
 
 // Minor runs svu minor.
 func Minor(ctx context.Context) error {
-	return svudagger.SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMinor))
+	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandMinor))
 }
 
 // Patch runs svu patch.
 func Patch(ctx context.Context) error {
-	return svudagger.SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandPatch))
+	return SVUWithOptions(ctx, svudagger.WithCommand(svudagger.CommandPatch))
+}
+
+// SVUWithOptions runs svu with specific options.
+//
+// TODO: Refactor this to make it more generic and reusable. Temporary solution to get svu working.
+//
+//nolint:revive // Stuttering is fine here to provide a functional options variant of SVU call.
+func SVUWithOptions(ctx context.Context, opts ...daggers.Option[svudagger.Config]) error {
+	verbose := mg.Verbose() || mg.Debug()
+
+	runtime, err := daggers.NewRuntime(ctx, daggers.WithVerbose(verbose))
+	if err != nil {
+		return err
+	}
+	defer runtime.Client.Close()
+
+	output, err := svudagger.Run(ctx, runtime, opts...)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(output.Version)
+
+	return nil
 }
