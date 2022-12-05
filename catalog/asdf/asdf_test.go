275d0939d4a23d120404fc0b3b5db566da1c6daf
aweris <ali@akca.io>
2022-12-05 11:57:44 +0100

    feat: add default version option to asdf plugin versions

--- catalog/asdf/asdf_test.go
+++ catalog/asdf/asdf_test.go
@@ -27,3 +27,16 @@ foo
 
 	assert.Equal(t, count, 1)
 }
+
+func TestPluginVersions_GetVersionOrDefault(t *testing.T) {
+	versions := PluginVersions{
+		"foo": {
+			Version:       "1.2.3",
+			VersionFreeze: true,
+		},
+	}
+
+	assert.Equal(t, versions.GetVersionOrDefault("foo", "v", "latest"), "v1.2.3")
+	assert.Equal(t, versions.GetVersionOrDefault("foo", "", "latest"), "1.2.3")
+	assert.Equal(t, versions.GetVersionOrDefault("bar", "v", "1.0.0"), "1.0.0")
+}
