ef7e2bf87d752ffdf7ef611c2f4caca136fd84cd
Ali AKCA <ali@akca.io>
2022-12-08 16:08:28 +0300

    chore: add readme and license (#76)

--- catalog/githubcli/doc.go
+++ catalog/githubcli/doc.go
@@ -1,2 +1,5 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package githubcli provides a command line interface to the GitHub CLI.
 package githubcli
--- daggers/containers/doc.go
+++ daggers/containers/doc.go
@@ -1,2 +1,5 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package containers provides a set of utilities to create and run containers.
 package containers
