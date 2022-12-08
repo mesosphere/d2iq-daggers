469a18f489e79e985dea75dcff941df23993d9d4
aweris <ali@akca.io>
2022-12-08 13:57:30 +0100

    chore: add license header to existing files

--- catalog/goreleaser/release/doc.go
+++ catalog/goreleaser/release/doc.go
@@ -1,2 +1,5 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package release provides a simple interface to the goreleaser release.
 package release
--- daggers/doc.go
+++ daggers/doc.go
@@ -1,2 +1,5 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package daggers provides a set of utilities to make it easier to work with dagger.
 package daggers
