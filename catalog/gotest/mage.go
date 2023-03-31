e068080b198972ba69e480e4ff6daa9e551b530e
Mikołaj Baranowski <mikolajb@gmail.com>
2023-03-31 15:18:54 +0200

    feat: Add gotest target to mage catalog
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

---
+++ catalog/gotest/mage.go
@@ -0,0 +1,71 @@
+package gotest
+
+import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+	"github.com/magefile/mage/mg"
+	"github.com/mesosphere/daggers/catalog/golang"
+	"github.com/mesosphere/daggers/daggers"
+	"github.com/mesosphere/daggers/daggers/containers"
+)
+
+const (
+	// EnvGowork env variable name for go.work.
+	EnvGowork = "GOWORK"
+	// EnvGoPrivate env variable name for GOPRIVATE.
+	EnvGoPrivate = "GOPRIVATE"
+)
+
+// Unit runs unit tests.
+func Gounit(ctx context.Context) error {
+	verbose := mg.Verbose() || mg.Debug()
+
+	runtime, err := daggers.NewRuntime(ctx, daggers.WithVerbose(verbose))
+	if err != nil {
+		return err
+	}
+
+	// golang container customizer options
+	customizers := golang.WithContainerCustomizers(
+		containers.WithGithubAuth(ctx),
+		containers.WithEnvVariables(map[string]string{
+			EnvGowork:    "off",
+			EnvGoPrivate: os.Getenv(EnvGoPrivate),
+		}),
+	)
+
+	// create a golang container
+	container, err := golang.GetContainer(ctx, runtime, customizers)
+	if err != nil {
+		return err
+	}
+
+	// execute the unit tests
+	return runUnitTests(ctx, container)
+}
+
+// runUnitTests runs the unit tests in the container and exports the test results to .output directory.
+func runUnitTests(ctx context.Context, container *dagger.Container) error {
+	testContainer := container.
+		WithExec([]string{"test", "-v", "-race", "-coverprofile", "coverage.txt", "-covermode", "atomic", "./..."}).
+		WithExec([]string{"tool", "cover", "-html=coverage.txt", "-o", "coverage.html"})
+
+	_, err := testContainer.ExitCode(ctx) // execute all steps and return the exit code
+	if err != nil {
+		return err
+	}
+
+	srcDir := testContainer.Directory("/src")
+
+	if _, err := srcDir.File("coverage.txt").Export(ctx, ".reports/coverage.txt"); err != nil {
+		return err
+	}
+
+	if _, err := srcDir.File("coverage.html").Export(ctx, ".reports/coverage.html"); err != nil {
+		return err
+	}
+
+	return nil
+}
--- magefile.go
+++ magefile.go
@@ -6,41 +6,9 @@
 package main
 
 import (
-	"context"
-
-	"github.com/magefile/mage/mg"
-
-	"github.com/mesosphere/daggers/catalog/golang"
-	"github.com/mesosphere/daggers/daggers"
-
 	// mage:import precommit
 	_ "github.com/mesosphere/daggers/catalog/precommit"
+
+	// mage:import test
+	_ "github.com/mesosphere/daggers/catalog/gotest"
 )
-
-// Test is a collection of test targets.
-//
-//goland:noinspection GoUnusedExportedType // used by mage
-type Test mg.Namespace
-
-func (Test) Go(ctx context.Context) error {
-	runtime, err := daggers.NewRuntime(ctx, daggers.WithVerbose(true))
-
-	args := []string{"test", "-v", "-race", "-coverprofile", "coverage.txt", "-covermode", "atomic", "./..."}
-
-	_, dir, err := golang.RunCommand(
-		ctx,
-		runtime,
-		golang.WithArgs(args...),
-		golang.WithEnv(map[string]string{"GOPRIVATE": "github.com/mesosphere"}),
-	)
-	if err != nil {
-		return err
-	}
-
-	_, err = dir.File("coverage.txt").Export(ctx, ".output/coverage.txt")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
